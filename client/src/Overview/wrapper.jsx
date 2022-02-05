58c4302dd1a697bbfaa5463116bab07f65fc8366
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-02-05 16:47:05 -0600

    Create folder and wrapper component for each module.  Render to parent App component

---
+++ client/src/Overview/wrapper.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h2>Product Overview Component</h2>
+    )
+  }
+}
+
+export default Overview;
---
+++ client/src/QA/wrapper.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class QAndA extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h2>Questions and Answers Component</h2>
+    )
+  }
+}
+
+export default QAndA;
