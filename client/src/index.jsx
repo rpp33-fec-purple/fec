db1b5af3d2ebb1ac8c9a2000afbf801e31d646c8
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-02-05 17:18:14 -0600

    updated index.jsx to render components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Overview from './Overview/wrapper.jsx';
+import Reviews from './Reviews/wrapper.jsx';
+import RelatedItems from './RelatedItems/wrapper.jsx';
+import QAndA from './QA/wrapper.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <>
+      <h1>Product Detail Page</h1>
+      <Overview/>
+      <RelatedItems/>
+      <QAndA/>
+      <Reviews/>
+      </>
+    )
+  }
+}
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <App/>,
   document.getElementById('app')
 );
