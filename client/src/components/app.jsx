a1514b687bb73b2d7740513b8b64a5128e60c68a
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-02-06 21:19:37 -0600

    create store and update file structure

---
+++ client/src/components/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Overview from './Overview/wrapper.jsx';
+import Reviews from './Reviews/wrapper.jsx';
+import RelatedItems from './RelatedItems/wrapper.jsx';
+import QAndA from './QA/wrapper.jsx';
+
+
+
+function App() {
+
+  return(
+    <div>
+    <h1>Product Detail Page</h1>
+    <Overview/>
+    <RelatedItems/>
+    <QAndA/>
+    <Reviews/>
+    </div>
+  )
+}
+
+export default App;
