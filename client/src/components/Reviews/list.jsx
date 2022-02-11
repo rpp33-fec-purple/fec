b861b20ba3ad6ab798f26c04815b8c8d60453b1a
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-02-10 22:15:05 -0600

    create list component

---
+++ client/src/components/Reviews/list.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Reviews List</div>
+    )
+  }
+}
+
+export default List;
