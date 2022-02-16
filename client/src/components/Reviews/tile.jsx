44e9dca05fa2f68d4755c7453a099bc9e0933a09
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-02-15 22:44:07 -0600

    recommend displayed

--- client/src/components/Reviews/tile.jsx
+++ client/src/components/Reviews/tile.jsx
@@ -13,11 +13,16 @@ class Tile extends React.Component {
     let view;
     if (this.props.reviews.results) {
       let currentReview = this.props.reviews.results[1];
+      let recommend;
+      if (currentReview.recommend) {
+        recommend = <div><FontAwesomeIcon icon={faCheck} /> I recommend this product</div>
+      }
       view =
       <div>
         <div>{currentReview.reviewer_name}, {formatDate(currentReview.date)}</div>
         <div><b>{currentReview.summary}</b></div>
         <div>{currentReview.body}</div>
+        <div>{recommend}</div>
       </div>
     } else {
       view = <div>No Reviews Yet</div>
