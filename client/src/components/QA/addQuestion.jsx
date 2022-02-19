8c8d134db3252f38a8a82994e26cea9c30aed5fe
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-02-19 11:33:37 -0800

    WIP Modal Window for Adding Question

--- client/src/components/QA/addQuestion.jsx
+++ client/src/components/QA/addQuestion.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import QuestionModal from './Modals/addQuestionModal.jsx';
+import styled from 'styled-components';
+import GlobalStyle from './globalStyles/globalStyles.jsx'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // height: 10vh;
+`
+
+const Button = styled.button`
+  min-width: 80px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 18px;
+  cursor: pointer;
+`
 
 class AddQuestion extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      isModalShowing: false
+    }
     this.addAQ = this.addAQ.bind(this);
+    this.changeModalVisibilityState = this.changeModalVisibilityState.bind(this);
+  }
+
+  changeModalVisibilityState() {
+    this.setState({
+      isModalShowing: !this.state.isModalShowing
+    }, ()=> {
+      console.log('is modal showing in changestate func', this.state.isModalShowing)
+    })
   }
 
   addAQ(){
@@ -14,11 +46,27 @@ class AddQuestion extends React.Component {
 
   render() {
     return (
-      <div id='addQuestionButton'>
-        <button onClick={this.addAQ}>ADD A QUESTION +</button>
-      </div>
+      <Container>
+        <Button onClick={this.changeModalVisibilityState}>ADD A QUESTION +</Button>
+        <QuestionModal productName={this.props.productName} isModalShowing={this.state.isModalShowing} changeModalState={this.changeModalVisibilityState}/>
+        <GlobalStyle/>
+      </Container>
     )
   }
 }
 
 export default AddQuestion;
+
+
+
+
+// const Button = styled.button`
+//   min-width: 100px;
+//   padding: 16px 32px;
+//   border-radius: 4px;
+//   border: none;
+//   background: #141414;
+//   color: #fff;
+//   font-size: 24px;
+//   cursor: pointer;
+// `
