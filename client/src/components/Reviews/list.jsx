ac8bbddff265c1317514526fa9c8a7e964851cab
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-02-24 15:23:40 -0600

    update render to collapse list if no reviews

--- client/src/components/Reviews/list.jsx
+++ client/src/components/Reviews/list.jsx
@@ -30,29 +30,37 @@ class List extends React.Component {
     let meta = this.props.meta;
     let reviews = this.props.reviews;
     let reviewsDisplayed = 0;
-    if (reviews.results) {
-      view =
-      <div>
-       <label>{reviews.results.length} reviews, sorted by </label>
-       <select name="sort" id="sort">
-         <option value="relevance">relevance</option>
-         <option value="helpfulness">helpfulness</option>
-         <option value="newest">newest</option>
-       </select>
-       <ScrollableList>
-          {reviews.results.map((review) => {
-            while (reviewsDisplayed < this.state.reviewsToDisplay) {
-              let reviewIndex = reviewsDisplayed;
-              reviewsDisplayed++;
-              return (
-                <Tile key={reviewIndex} productID={this.props.productID} reviewIndex={reviewIndex} reviews={this.props.reviews} meta={this.props.meta}/>
-              )
-            }
-          })}
-       </ScrollableList>
-       <button style={{display: reviews.results.length > reviewsDisplayed ?  'inline' : 'none'}}onClick={this.handleMoreButtonClick}>MORE REVIEWS</button>
-     </div>
 
+    //check for props from initial api call
+    if (reviews.results) {
+      //If there are reviews for the item
+      if (reviews.results.length > 0) {
+        view =
+        <div>
+          <label>{reviews.results.length} reviews, sorted by </label>
+          <select name="sort" id="sort">
+            <option value="relevance">relevance</option>
+            <option value="helpfulness">helpfulness</option>
+            <option value="newest">newest</option>
+          </select>
+          <ScrollableList>
+              {reviews.results.map((review) => {
+                while (reviewsDisplayed < this.state.reviewsToDisplay) {
+                  let reviewIndex = reviewsDisplayed;
+                  reviewsDisplayed++;
+                  return (
+                    <Tile key={reviewIndex} productID={this.props.productID} reviewIndex={reviewIndex} reviews={this.props.reviews} meta={this.props.meta}/>
+                  )
+                }
+              })}
+          </ScrollableList>
+          <button style={{display: reviews.results.length > reviewsDisplayed ?  'inline' : 'none'}}onClick={this.handleMoreButtonClick}>MORE REVIEWS</button>
+        </div>
+      //there are no reviews yet
+      } else {
+        view = null;
+      }
+    //Initial api call not finished yet
     } else {
       view = null;
     }
