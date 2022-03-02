d5f25ef8285c7262a962336f87f89cbbc78f1463
Jeff Sparrow <jeffsparrow.dev@gmail.com>
2022-03-02 12:43:11 -0600

    rough new review form without photo upload or submisson logic

--- client/src/components/Reviews/newReview.jsx
+++ client/src/components/Reviews/newReview.jsx
@@ -61,9 +61,9 @@ class NewReview extends React.Component {
     }
     let characteristicRating = characteristics.map((char) => {
       return (
-        <li>
+        <li key={char}>
           <label>{char}</label>
-          <input type="radio" id={char} name={char} value="1"/>
+          <input type="radio" id={char} name={char} value="1" required/>
           <label htmlFor={char}>{this.characteristicOptions[char][1]}</label>
           <input type="radio" id={char} name={char} value="2"/>
           <label htmlFor={char}>{this.characteristicOptions[char][2]}</label>
@@ -93,12 +93,34 @@ class NewReview extends React.Component {
           </li>
           <li>
             <label htmlFor="">Do you recommend this product?*</label>
-            <input type="radio" id="yes" name="recommend" value="yes"/>
+            <input type="radio" id="yes" name="recommend" value="yes" required/>
             <label htmlFor="yes">Yes</label>
             <input type="radio" id="no" name="recommend" value="no"/>
             <label htmlFor="no">No</label>
           </li>
+          <label>Characteristics*</label>
           {characteristicRating}
+          <li>
+            <label htmlFor="summary">Review summary</label>
+            <textarea id="summary" name="summary" maxLength="60"rows="3" cols="33" placeholder="Example: Best purchase ever!"/>
+          </li>
+          <li>
+            <label htmlFor="body">Review body*</label>
+            <textarea id="body" name="body" minLength="50" maxLength="1000"rows="8" cols="33" placeholder="Why did you like the product or not?" required/>
+          </li>
+          <li>
+            <label>Upload your photos</label>
+          </li>
+          <li>
+            <label htmlFor="nickname">What is your nickname*</label>
+            <input type="text" id="nickname" name="nickname" maxLength="60" placeholder="Example: jackson11!" required/>
+            <label htmlFor="nickname">For privacy reasons, do not use your full name or email address</label>
+          </li>
+          <li>
+            <label htmlFor="email">Your email*</label>
+            <input type="email" id="email" name="email" maxLength="60" placeholder="Example: jackson11@email.com" required/>
+            <label htmlFor="nickname">For authentication reasons, you will not be emailed</label>
+          </li>
           <li>
             <input type="submit" id="submit" name=""/>
           </li>
