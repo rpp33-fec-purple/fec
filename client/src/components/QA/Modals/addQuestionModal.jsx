3e28d7fd34ba4a5831707e0ad1f36a0f90b3c2a0
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-03-02 16:22:01 -0800

    fixed opacity/transparency bug concerning add question modal window

--- client/src/components/QA/Modals/addQuestionModal.jsx
+++ client/src/components/QA/Modals/addQuestionModal.jsx
@@ -4,14 +4,27 @@ import styled from 'styled-components';
 import { MdClose } from 'react-icons/md';
 
 const Background = styled.div`
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.8);
-  position: absolute;
-  // position: fixed;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
   display: flex;
+  background: rgba(0, 0, 0, 0.8);
   justify-content: center;
   align-items: center;
+
+
+
+  // width: 100%;
+  // height: 100%;
+  // background: rgba(0, 0, 0, 0.8);
+  // position: absolute;
+  // // position: fixed;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
 `;
 const ModalWrapper = styled.div`
   width: 600px;
